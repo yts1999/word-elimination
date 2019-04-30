ed8898c3187511175abee7fd5201fe1fac7a0ae5
yts1999 <yts1999@yeah.net>
2019-04-30 16:39:11 +0800

    modify userdb

--- userdb.cpp
+++ userdb.cpp
@@ -5,26 +5,26 @@
 #include <QSqlQuery>
 #include <QVariant>
 
-UserDatabase::UserDatabase() {
-    userdb = QSqlDatabase::addDatabase("QSQLITE","userdb");
-    userdb.setHostName("localhost");
-    userdb.setDatabaseName("users.db");
-    if (! userdb.open()) {
-        QMessageBox::critical(nullptr, "错误", "连接错误！");
+UserDatabase::UserDatabase() :
+    QSqlDatabase (addDatabase("QSQLITE", "userdb")) {
+        setHostName("localhost");
+        setDatabaseName("users.db");
+        if (! open()) {
+            QMessageBox::critical(nullptr, "错误", "连接错误！");
+        }
+        else {
+            QSqlQuery userdb_create(*this);
+            userdb_create.exec("create table user(name varchar(10) primary key, passwd varchar(10))");
+        }
     }
-    else {
-        QSqlQuery userdb_create(userdb);
-        userdb_create.exec("create table user(name varchar(10) primary key, passwd varchar(10))");
-    }
-}
 
 UserDatabase::~UserDatabase() {
-    userdb.close();
-    userdb.removeDatabase("userdb");
+    close();
+    removeDatabase("userdb");
 }
 
-int UserDatabase::check_usr_pwd(QString usr,QString pwd) {
-    QSqlQuery userdb_query(userdb);
+int UserDatabase::check_usr_pwd(const QString &usr,const QString &pwd) const {
+    QSqlQuery userdb_query(*this);
     userdb_query.exec(QString("select * from user where name=='%1'").arg(usr));
     if (! userdb_query.first())
         return 1;
--- userdb.h
+++ userdb.h
@@ -4,14 +4,12 @@
 #include <QSqlDatabase>
 #include <QString>
 
-class UserDatabase {
+class UserDatabase : public QSqlDatabase {
 public:
        UserDatabase();
        ~UserDatabase();
-       int check_usr_pwd(QString usr,QString pwd); //返回0表示正确，返回1表示用户名错误，返回2表示密码错误
+       int check_usr_pwd(const QString &usr,const QString &pwd) const; //返回0表示正确，返回1表示用户名错误，返回2表示密码错误
 
-private:
-       QSqlDatabase userdb;
 };
 
 #endif // USERDB_H
